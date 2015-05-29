63c7a8220597cdf1d54562b6e4f062347d9117d6
KallePower <kalle.muenster@gmx.de>
2015-05-29 22:14:04 +0200

    camera rotating with character

--- inno-GameJam 8 2015/Assets/Rotator.cs
+++ inno-GameJam 8 2015/Assets/Rotator.cs
@@ -3,19 +3,21 @@ using System.Collections;
 
 public class Rotator : MonoBehaviour {
 
+	public Transform characterTransform;
 	public Vector3 TargetDirection = new Vector3(1,0,0);
 	public Vector3 currentDirection = new Vector3(1,0,0);
 	
 	public float speed = 10;
 	public float angel = 0;
-	private bool isRotating = false;
+	private bool isRotating=false;
 	private Vector3 startDirection = new Vector3(1, 0, 0);
 	private float timer;
-	public float rotationTime = 1;
+	public float rotationTime = 2;
 
 	void Start() 
 	{
-	
+		characterTransform = GameObject.Find("wizard").GetComponent<Transform>();
+		transform.right = TargetDirection = characterTransform.forward;
 	}
 
 	private void rotate()
@@ -23,13 +25,18 @@ public class Rotator : MonoBehaviour {
 		if(!isRotating)
 			startDirection = currentDirection;
 		timer += Time.deltaTime;
-		currentDirection = Vector3.Lerp(startDirection, TargetDirection, timer / rotationTime);
-
+		transform.right = -(currentDirection = Vector3.Lerp(startDirection, TargetDirection, timer / rotationTime));
+		if(currentDirection == TargetDirection)
+		{
+			isRotating = false;
+			timer = 0;
+		}
 	}
 	
 	// Update is called once per frame
 	void Update() 
 	{
+		TargetDirection = characterTransform.forward;
 		if(TargetDirection != currentDirection)
 			rotate();
 	}

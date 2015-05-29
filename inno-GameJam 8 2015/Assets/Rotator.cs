9bea52207d88dd780bc0c6175874056d89ee14dc
KallePower <kalle.muenster@gmx.de>
2015-05-29 20:46:04 +0200

    sceneblender

---
+++ inno-GameJam 8 2015/Assets/Rotator.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+
+	public Vector3 TargetDirection = new Vector3(1,0,0);
+	public Vector3 currentDirection = new Vector3(1,0,0);
+	
+	public float speed = 10;
+	public float angel = 0;
+	private bool isRotating = false;
+	private Vector3 startDirection = new Vector3(1, 0, 0);
+	private float timer;
+	public float rotationTime = 1;
+
+	void Start() 
+	{
+	
+	}
+
+	private void rotate()
+	{
+		if(!isRotating)
+			startDirection = currentDirection;
+		timer += Time.deltaTime;
+		currentDirection = Vector3.Lerp(startDirection, TargetDirection, timer / rotationTime);
+
+	}
+	
+	// Update is called once per frame
+	void Update() 
+	{
+		if(TargetDirection != currentDirection)
+			rotate();
+	}
+}

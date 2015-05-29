0eae42a4a693629149cb11dbc3d98a76f941556e
KallePower <kalle.muenster@gmx.de>
2015-05-30 01:31:36 +0200

    door opens and closes

---
+++ inno-GameJam 8 2015/Assets/GameMasterScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameMasterScript : MonoBehaviour 
+{
+	private static GameMasterScript _instance = null;
+	public static GameMasterScript Instance
+	{
+		get { return _instance; }
+	}
+
+	void Awake()
+	{
+		if(_instance != null && _instance != this)
+		{
+			Destroy(gameObject);
+			return;
+		}
+
+		_instance = this;
+
+		DontDestroyOnLoad(gameObject);
+	}
+
+
+	// Use this for initialization
+	void Start()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+	
+	}
+}
---
+++ inno-GameJam 8 2015/Assets/OpenDoor.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpenDoor : MonoBehaviour {
+
+	private bool isAnimating = false;
+	[SerializeField]
+	private bool open = false;
+	private bool isOpen = false;
+	public bool Open
+	{
+		get { return open; }
+		set
+		{
+			 if(value!=isOpen && !isAnimating)
+			 {
+				 isAnimating = true;
+				 timer = 0;
+			 }
+
+			 if(value == isOpen)
+				 isAnimating = false;
+			 open = value;
+		}
+	}
+
+	private Vector3 START,END;
+	private float timer=0;
+	public float speed = 3;
+
+
+	private float closed;
+
+	bool AnimateDoor(bool shouldDoorBeOpen)
+	{
+		if(isAnimating)
+		{
+			float eulerY;
+		//	timer += Time.deltaTime;
+			transform.Rotate(new Vector3(0, 0, 1), shouldDoorBeOpen ? speed* Time.deltaTime : -speed*Time.deltaTime);
+			eulerY = transform.eulerAngles.y;
+
+			if(eulerY <= closed || eulerY >= closed+115)
+			return shouldDoorBeOpen;
+
+			if(timer >= speed)
+				return shouldDoorBeOpen;
+		}
+
+		return !shouldDoorBeOpen;
+	}
+
+
+
+	void Start()
+	{
+		closed = transform.eulerAngles.y;
+	}
+	
+
+	void Update()
+	{
+		Open = open;
+		if (Open != isOpen)
+		{
+			isOpen = AnimateDoor(Open);
+		}
+	}
+}

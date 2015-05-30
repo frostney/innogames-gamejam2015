093d34e6f786bb3935c65bcc0ef529f1cf3fff55
AndreAhrendt <andre.ahrendt@web.de>
2015-05-30 16:07:04 +0200

    Character_script
    add more movements, not finished

--- inno-GameJam 8 2015/Assets/Resources/Scripts/character.cs
+++ inno-GameJam 8 2015/Assets/Resources/Scripts/character.cs
@@ -58,34 +58,29 @@ public class character : MonoBehaviour
 			
 		}
 
+		int z = 0;
+		int rx = 0;
+		int y = 0;
+		int ry = 0;
+		switch(pos)
+		{
+		case 0: y = -10 + (int)pos_char.y;
+			break;
+		case 1: z = 10 + (int)pos_char.z;
+			break;
+		case 2: y = 10 + (int)pos_char.y;
+			break;
+		case 3: z = -10 + (int)pos_char.z;
+			break;
+		}
+
 		RaycastHit hit;
-		Ray rayX = new Ray(pos_char, new Vector2(pos_char.x + 10, 0));
-		Physics.Raycast(rayX, out hit, max_distance_wall);
-		if (hit.collider != null && hit.collider.gameObject.layer == LayerMask.NameToLayer(ground_layer))
-		{
-			Vector3 rotate = this.gameObject.transform.eulerAngles;
-			this.gameObject.transform.eulerAngles = new Vector3(rotate.x, rotate.y, rotate.z + 90);
-			pos++;
-			if (pos > 3)
-				pos = 0;
-		}
-
-		rayX = new Ray(pos_char, new Vector2(pos_char.x - 10, 0));
-		Physics.Raycast(rayX, out hit, max_distance_wall);		
-		if (hit.collider != null && hit.collider.gameObject.layer == LayerMask.NameToLayer(ground_layer))
-		{
-			Vector3 rotate = this.gameObject.transform.eulerAngles;
-			this.gameObject.transform.eulerAngles = new Vector3(rotate.x, rotate.y, rotate.z - 90);
-			pos--;
-			if (pos < 0)
-				pos = 3;
-		}
-
-		rayX = new Ray(pos_char, new Vector2(0, pos_char.y - 10));
+		float temp_distance = 0f;
+		Ray rayX = new Ray(pos_char, new Vector3(0,y,z));
 		Physics.Raycast(rayX, out hit, max_distance_ground);
 		if (hit.collider == null && !falling)
 		{
-			SetFalling();
+			;//SetFalling();
 		}
 		else if (hit.collider != null && hit.collider.gameObject.layer == LayerMask.NameToLayer(ground_layer) && falling)
 		{
@@ -93,17 +88,149 @@ public class character : MonoBehaviour
 			animation.Play();
 			falling = false;
 		}
-	}
+		else
+		{
+			temp_distance = hit.distance;
+			Debug.Log ("temp:" + temp_distance);
+		}
+
+
+		/*
+		x = 0;
+		y = 0;
+		switch(pos)
+		{
+		case 0: x = -10 + (int)pos_char.x;
+			rx = -90;
+			break;
+		case 1: y = -10 + (int)pos_char.y;
+			ry = -90;
+			break;
+		case 2: x = 10 + (int)pos_char.x;
+			rx = 90;
+			break;
+		case 3: y = 10 + (int)pos_char.y;
+			ry = 90;
+			break;
+		}
+
+		rayX = new Ray(pos_char, new Vector2(x,y));
+		Physics.Raycast(rayX, out hit, max_distance_wall);
+		if (hit.collider != null && hit.collider.gameObject.layer == LayerMask.NameToLayer(ground_layer) && moving_right)
+		{
+			Vector3 rotate = this.gameObject.transform.eulerAngles;
+			this.gameObject.transform.eulerAngles = new Vector3(rotate.x +rx, rotate.y +ry, rotate.z);
+			rotate_z -= 90;
+			ClimbingCorrection(temp_distance, -1);
+			pos++;
+			if (pos > 3)
+				pos = 0;
+		}*/
+/*
+		rayX = new Ray(pos_char, new Vector2(pos_char.x - 10, 0));
+		Physics.Raycast(rayX, out hit, max_distance_wall);		
+		if (hit.collider != null && hit.collider.gameObject.layer == LayerMask.NameToLayer(ground_layer) && moving_left)
+		{
+			Vector3 rotate = this.gameObject.transform.eulerAngles;
+			this.gameObject.transform.eulerAngles = new Vector3(rotate.x - 90, rotate.y, rotate.z);
+			ClimbingCorrection(temp_distance, -1);
+			pos--;
+			if (pos < 0)
+				pos = 3;
+		}*/
 		
+		CollisionSideTest(1, pos_char, temp_distance);
+		CollisionSideTest(-1, pos_char, temp_distance);
+	}
+
+	private void CollisionSideTest(short direction, Vector3 pos_char, float temp_distance)
+	{
+		bool moving = direction == 1 ? moving_right : moving_left;
+		int x = 0;
+		int y = 0;
+		switch(pos)
+		{
+		case 0: x = 10 * direction;
+			x+= (int)pos_char.x;
+			break;
+		case 1: y = 10 * direction;
+			y+= (int)pos_char.y;
+			break;
+		case 2: x = -10 * direction;
+			x+= (int)pos_char.x;
+			break;
+		case 3: y = -10 * direction;
+			y+= (int)pos_char.y;
+			break;
+		}
+
+		RaycastHit hit;
+		Ray rayX = new Ray(pos_char, new Vector2(x, y));
+		Physics.Raycast(rayX, out hit, max_distance_wall);
+		if (hit.collider != null && hit.collider.gameObject.layer == LayerMask.NameToLayer(ground_layer) && moving)
+		{
+			Vector3 rotate = this.gameObject.transform.eulerAngles;
+			this.gameObject.transform.eulerAngles = new Vector3(rotate.x - 90 * direction, rotate.y, rotate.z );
+			rotate_z -= 90 * direction;
+
+			ClimbingCorrection(temp_distance, direction);
+			pos += direction;
+			if (pos > 3)
+				pos = 0;
+		
+			if (pos < 0)
+				pos = 3;
+		}
+	}
+
+	private void ClimbingCorrection(float distance, short direction)
+	{
+		float temp = 0f;
+		int x = 0;
+		int y = 0;
+		switch(pos)
+		{
+		case 0: x = 10 * direction;
+			break;
+		case 1: y = -10 * direction;
+			break;
+		case 2: x = -10 * direction;
+			break;
+		case 3: y = 10 * direction;
+			break;
+		}
+
+		bool run = true;
+		RaycastHit hit;
+		while(run)
+		{
+			Vector3 pos_char = this.gameObject.transform.localPosition;
+			Ray rayX = new Ray(pos_char, new Vector2(x, y));
+			Physics.Raycast(rayX, out hit, max_distance_wall);
+			if (hit.collider != null && hit.collider.gameObject.layer == LayerMask.NameToLayer(ground_layer))
+			{Debug.Log (hit.distance);
+				temp = hit.distance;
+				this.gameObject.transform.Translate(0, (distance - temp), 0);
+
+				/*falling = true;
+				moving_left = false;
+				moving_right = false;*/
+
+				run = false;
+			}
+			else
+			{
+				run = false;
+				this.gameObject.transform.Translate(0, distance , 0);
+			}
+		}
+	}
+
 	private void Move()
 	{
-		if (moving_left)
+		if (moving_left || moving_right)
 		{
-			this.gameObject.transform.Translate(0, 0, Time.deltaTime * walk_speed);
-		}
-		else if (moving_right)
-		{
-			this.gameObject.transform.Translate(0, 0, Time.deltaTime * walk_speed);
+			this.gameObject.transform.Translate( 0, 0, Time.deltaTime * walk_speed);
 		}
 	}
 
@@ -163,11 +290,11 @@ public class character : MonoBehaviour
 		{
 		case 0: y = -90 * direction;
 			break;
-		case 1: x = 90 * direction;
+		case 1: x = -90 * direction;
 			break;
 		case 2: y = 90 * direction;
 			break;
-		case 3: x = -90 * direction;
+		case 3: x = 90 * direction;
 			break;
 		}
 		

e26caa08a17d088c3f6c998d1f29de629280e1cd
Johannes Stein <johannesstein@freeze-dev.com>
2015-05-30 23:35:56 +0200

    Add tutorial strings to scenes

---
+++ inno-GameJam 8 2015/Assets/Resources/Scripts/TutorialHintMove.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TutorialHintMove : MonoBehaviour {
+
+	void OnGUI() {
+		GUI.Label (new Rect (10, Screen.height - 40 - 10, 250, 40), "Press LEFT and RIGHT to move around.\nOh, and you can run up walls.");
+	}
+
+}

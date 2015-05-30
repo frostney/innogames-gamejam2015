47cd4b62866ffb62e46ab01a8fc920d51cfc2fc8
AndreAhrendt <andre.ahrendt@web.de>
2015-05-30 03:25:11 +0200

    Character_move_script
    moving around included

--- inno-GameJam 8 2015/Assets/Resources/Scripts/character.cs
+++ inno-GameJam 8 2015/Assets/Resources/Scripts/character.cs
@@ -10,6 +10,9 @@ public class character : MonoBehaviour
 	bool looking_left = false;
 	bool falling = false;
 	float rotate_y = 0f;
+	float rotate_x = 0f;
+	float rotate_z = 0f;
+	short pos = 0;
 
 	[SerializeField]
 	public string animation_idle = "None";
@@ -38,13 +41,20 @@ public class character : MonoBehaviour
 	void Update () 
 	{
 		InputCheck();
-		CollissionCheck();
+		CollisionCheck();
 		Move();
 	}
 
 	private void CollisionCheck()
 	{
-
+		if (falling)
+		{
+			Vector3 camera_rotation = Camera.main.transform.eulerAngles;
+			Camera.main.transform.eulerAngles = new Vector3(camera_rotation.x,camera_rotation.y, camera_rotation.z + 90);
+			Vector3 rotate = this.gameObject.transform.eulerAngles;
+			this.gameObject.transform.eulerAngles = new Vector3(rotate.x, rotate.y, rotate.z + 90);
+			falling = false;
+		}
 	}
 
 	private void Move()
@@ -55,11 +65,11 @@ public class character : MonoBehaviour
 		}
 		else if (moving_left)
 		{
-
+			this.gameObject.transform.Translate(0, 0, -Time.deltaTime * walk_speed);
 		}
 		else if (moving_right)
 		{
-
+			this.gameObject.transform.Translate(0, 0, -Time.deltaTime * walk_speed);
 		}
 	}
 
@@ -68,7 +78,7 @@ public class character : MonoBehaviour
 		if (Input.GetKeyUp(right) && moving_right)
 		{
 			Vector3 rotate = this.gameObject.transform.eulerAngles;
-			this.gameObject.transform.eulerAngles = new Vector3(rotate.x, rotate_y, rotate.z);
+			this.gameObject.transform.eulerAngles = new Vector3(rotate_x, rotate_y, rotate_z);
 			animation.clip = animation.GetClip(animation_idle);
 			animation.Play();
 			moving_right = false;
@@ -77,43 +87,64 @@ public class character : MonoBehaviour
 		else if (Input.GetKeyUp(left) && moving_left)
 		{
 			Vector3 rotate = this.gameObject.transform.eulerAngles;
-			this.gameObject.transform.eulerAngles = new Vector3(rotate.x, rotate_y, rotate.z);
+			this.gameObject.transform.eulerAngles = new Vector3(rotate_x, rotate_y, rotate_z);
 			animation.clip = animation.GetClip(animation_idle);
 			animation.Play();
 			moving_left = false;
-			looking_left = false;;
+			looking_left = false;
 		}
 		
 		if (Input.GetKeyDown(release))
 		{
 			Vector3 rotate = this.gameObject.transform.eulerAngles;
-			this.gameObject.transform.eulerAngles = new Vector3(rotate.x, rotate_y, rotate.z);
+			this.gameObject.transform.eulerAngles = new Vector3(rotate_x, rotate_y, rotate_z);
 			animation.clip = animation.GetClip(animation_fall);
 			animation.Play();
 			moving_left = false;
 			moving_right = false;
 			falling = true;
+			pos++;
+			if (pos > 3)
+				pos = 0;
 		}
 		
 		if (Input.GetKeyDown(right) && !moving_left && !falling)
 		{
-			Vector3 rotate = this.gameObject.transform.eulerAngles;
-			rotate_y = rotate.y;
-			this.gameObject.transform.eulerAngles = new Vector3(rotate.x, rotate_y - 90, rotate.z);
-			animation.clip = animation.GetClip(animation_walk);
-			animation.Play();
+			SetRotation(1);
 			moving_right = true;
 			looking_right = true;
 		}
 		else if (Input.GetKeyDown(left) && !moving_right && !falling)
 		{
-			Vector3 rotate = this.gameObject.transform.eulerAngles;
-			rotate_y = rotate.y;
-			this.gameObject.transform.eulerAngles = new Vector3(rotate.x, rotate_y + 90, rotate.z);
-			animation.clip = animation.GetClip(animation_walk);
-			animation.Play();
+			SetRotation(-1);
 			moving_left = true;
 			looking_left = true;
 		}
 	}
-}
+
+	private void SetRotation(short direction)
+	{
+		Vector3 rotate = this.gameObject.transform.eulerAngles;
+		rotate_y = rotate.y;
+		rotate_x = rotate.x;
+		rotate_z = rotate.z;
+		int x = 0;
+		int y = 0;
+		switch(pos)
+		{
+		case 0: y = -90 * direction;
+			break;
+		case 1: x = 90 * direction;
+			break;
+		case 2: y = 90 * direction;
+			break;
+		case 3: x = -90 * direction;
+			break;
+		}
+		
+		this.gameObject.transform.eulerAngles = new Vector3(rotate_x + x, rotate_y + y, rotate_z);
+		animation.clip = animation.GetClip(animation_walk);
+		animation.Play();
+	}
+	}
+	

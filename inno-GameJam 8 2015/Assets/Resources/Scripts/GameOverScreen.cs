2e9bd6479e1bf8ee83854d05ff8a13b894fedaf0
Johannes Stein <johannesstein@freeze-dev.com>
2015-05-31 00:12:27 +0200

    GameOver screen

---
+++ inno-GameJam 8 2015/Assets/Resources/Scripts/GameOverScreen.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameOverScreen : MonoBehaviour {
+
+	void OnGUI() {
+		if (GUI.Button(new Rect(((Screen.width - 150) / 2), Screen.height - 50 - 50, 150, 50), "Click to retry"))
+		{
+			Application.LoadLevel("Intro");
+		}
+	}
+}

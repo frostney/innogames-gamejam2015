21417b508795b393fc661ca4b6c4b8083348b78a
Johannes Stein <johannesstein@freeze-dev.com>
2015-05-31 09:15:36 +0200

    Update button label in GameOverScreen

--- inno-GameJam 8 2015/Assets/Resources/Scripts/GameOverScreen.cs
+++ inno-GameJam 8 2015/Assets/Resources/Scripts/GameOverScreen.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class GameOverScreen : MonoBehaviour {
 
 	void OnGUI() {
-		if (GUI.Button(new Rect(((Screen.width - 150) / 2), Screen.height - 50 - 50, 150, 50), "Click to retry"))
+		if (GUI.Button(new Rect(((Screen.width - 150) / 2), Screen.height - 50 - 50, 150, 50), "Play again?"))
 		{
 			Application.LoadLevel("Intro");
 		}

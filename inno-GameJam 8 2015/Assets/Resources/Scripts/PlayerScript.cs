65fb99df7d3b40d8f9b04a5a05918ccb6aaacbb7
KallePower <kalle.muenster@gmx.de>
2015-05-31 13:10:12 +0200

    laufen geht

--- inno-GameJam 8 2015/Assets/Resources/Scripts/PlayerScript.cs
+++ inno-GameJam 8 2015/Assets/Resources/Scripts/PlayerScript.cs
@@ -13,7 +13,7 @@ public class PlayerScript : MonoBehaviour
 
 	[SerializeField]
 	private Vector3 Right,Left;
-//	private GameObject wizard;
+	private Transform innerTransform;
 
 
 	public DIRECTION MovingDirection;
@@ -21,10 +21,10 @@ public class PlayerScript : MonoBehaviour
 	void Start() 
 	{
 		GetComponent<Rigidbody>().centerOfMass =  -this.transform.up * 5;
-	//	wizard = transform.GetChild(0).gameObject;
+		innerTransform = transform.GetChild(0);
 	}
 
-	public float speed = 1;
+	public float speed = 3;
 	private float Speed
 	{
 		get { return (speed * Time.deltaTime); }
@@ -58,16 +58,21 @@ public class PlayerScript : MonoBehaviour
 			switch(newDirection)
 			{
 			case DIRECTION.LEFT:
-				//transform.eulerAngles = new Vector3(transform.eulerAngles.x, 90.0f, transform.eulerAngles.z);
+			//	innerTransform.up = this.transform.up;
+			//	innerTransform.forward = this.transform.right;
 		
 				break;
 
 			case DIRECTION.STAND:
+				innerTransform.rotation = this.transform.rotation;
+				innerTransform.localEulerAngles = new Vector3( innerTransform.localEulerAngles.x,180, innerTransform.localEulerAngles.z);
+
 				//transform.eulerAngles = new Vector3(transform.eulerAngles.x, 180.0f, transform.eulerAngles.z);
 
 				break;
 
 			case DIRECTION.RIGHT:
+			//	innerTransform.forward = -this.transform.right;
 				//transform.eulerAngles = new Vector3(transform.eulerAngles.x, -90.0f, transform.eulerAngles.z);
 			
 				break;
@@ -79,11 +84,30 @@ public class PlayerScript : MonoBehaviour
 
 	private void moveCharackter()
 	{
+		switch (MovingDirection)
+		{
+		case DIRECTION.LEFT:
+			innerTransform.rotation = this.transform.rotation;
+			innerTransform.localEulerAngles = new Vector3(innerTransform.localEulerAngles.x, -90.0f, innerTransform.localEulerAngles.z);
+		//	innerTransform.up = this.transform.up;
+		//	innerTransform.forward = -this.transform.right;
+			this.transform.position += ( -this.transform.right * Speed );
+			break;
 
-		if(MovingDirection == DIRECTION.LEFT)
-			transform.position += ( transform.right * Speed );
-		else if(MovingDirection == DIRECTION.RIGHT)
-			transform.position += ( -transform.right * Speed );
+		case DIRECTION.STAND:
+
+			break;
+
+		case DIRECTION.RIGHT:
+			innerTransform.rotation = this.transform.rotation;
+			innerTransform.localEulerAngles = new Vector3(innerTransform.localEulerAngles.x, 90.0f, innerTransform.localEulerAngles.z);
+		//	innerTransform.up = this.transform.up;
+		//	innerTransform.forward = this.transform.right;
+			this.transform.position += ( this.transform.right * Speed );
+			break;
+		}
+		
+			
 	}
 	
 	// Update is called once per frame
